65136d8ee2ac98a420a8d0f096f6cf856974c9c9
oliveprice <141072289+oliveprice@users.noreply.github.com>
2025-04-22 14:44:07 -0700

    test
    test

---
+++ scripts/moving-parts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const angel = document.getElementById('angel-lottie');
+
+  const animation = lottie.loadAnimation({
+    container: angel,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './resources/animations/angel/data.json'
+  });
+
+  animation.setSpeed(1); // adjust this if needed
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const octo = document.getElementById('octo-lottie');
+
+  const animation = lottie.loadAnimation({
+    container: octo,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './resources/animations/octopus/data.json'
+  });
+
+  animation.setSpeed(1);
+});
+
